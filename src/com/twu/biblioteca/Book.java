3e39e48477ce3bb8ae0d463f198dbf1b891c58d4
Thalia Estrella <thaliaestrella12@gmail.com>
2019-08-16 14:42:43 -0500

    Made Book and BookTests class

---
+++ src/com/twu/biblioteca/Book.java
@@ -0,0 +1,8 @@
+package com.twu.biblioteca;
+
+public class Book {
+
+    public Book(){
+
+    }
+}
---
+++ test/com/twu/biblioteca/BookTest.java
@@ -0,0 +1,12 @@
+package com.twu.biblioteca;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class BookTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
